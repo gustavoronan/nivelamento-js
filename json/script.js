46b9a8ed00566e3478c20d17f7b77d22f8573fc9
Gustavo Ronan <gustavoronn@gmail.com>
2025-01-02 21:10:16 -0300

    json

---
+++ json/script.js
@@ -0,0 +1,26 @@
+const produto = {
+    nome: "produto 1",
+    preco: 3000.00
+}
+
+const obj1 = {
+    id: 4,
+    date: "2020-01-02",
+    items : [
+        {
+            description: "Celular",
+            price: 1200.00,
+            quantity: 1
+        },
+        {
+            description: "MacBook",
+            price: 2000.00,
+            quantity: 2
+        }
+    ], 
+    client: {
+        name: "Gustavo Ronan", 
+        email: "gustavoronn@gmail.com",
+        active: true
+    }
+}
