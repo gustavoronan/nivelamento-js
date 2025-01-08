b4a6fb1b42786f84c4a93c01e69e86c540fe42d3
Gustavo Ronan <gustavoronn@gmail.com>
2025-01-07 22:42:52 -0300

    desafio terceirizados concluido

--- json/terceirizados/script.js
+++ json/terceirizados/script.js
@@ -39,7 +39,14 @@ const dados = [
   //---------------------------------------------------------------------------
   
   function funcionariosTerceirizados(list) {
-  
+    const result = [] //criando uma lista vazia
+
+    for (let i = 0; i< list.length; i++){ //percorrendo todos os funcionarios da lista 
+        if (list[i].terceirizado) //verificando se a propriedade terceirizado é true
+        result.push(list[i])//se sim, adiciona a lista result
+    }
+
+    return result //retorna a lista de result preenchida com funcionarios terceirizados
   }
   
   //---------------------------------------------------------------------------
@@ -50,16 +57,21 @@ const dados = [
   //---------------------------------------------------------------------------
   
   function custoTotal(list) {
-  
+    let total = 0;
+    for (let i = 0; i < list.length; i++) {
+      const subTotal = list[i].valorHora * list[i].horasTrabalhadas; //calculando o subtotal de cada funcionario
+      total = total + subTotal; //adicionando o subtotal ao total
+    }
+    return total; //retornando o valor final
   }
   
   //---------------------------------------------------------------------------
   // SCRIPT PRINCIPAL
   //---------------------------------------------------------------------------
   
-  const terceirizados = funcionariosTerceirizados(dados);
+  const terceirizados = funcionariosTerceirizados(dados); //retorno dos funcionarios terceirizados
   
-  const custoTerceirizados = custoTotal(terceirizados);
+  const custoTerceirizados = custoTotal(terceirizados); //custo total apenas dos funcionarios terceirizados
   
   console.log(
     "Custo total dos funcionarios terceirizados: R$ " + custoTerceirizados
