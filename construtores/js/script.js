e696cbfdd6e23f62159ba09b28f58dff76d835e5
Gustavo Ronan <gustavoronn@gmail.com>
2025-01-09 12:18:00 -0300

    Add: funcoes de construtores e prototype

---
+++ construtores/js/script.js
@@ -0,0 +1,39 @@
+const p1 = {
+    name: "Computador",
+    price: 2000.0,
+    quantity: 2
+}
+
+const Product = function(name, price, quantity){
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+}
+
+Product.prototype.total = function(){ //trabalha com uma funcao dentro do prototype do objeto
+    return this.price * this.quantity;
+}
+
+Product.prototype.add = function(amount){
+    this.quantity += amount
+}
+
+Product.prototype.remove = function(amount){
+    if(this.quantity >= amount){
+
+        this.quantity -= amount
+    }
+}
+
+Product.prototype.label = function() {
+    return "Dados: " + this.name + "," + this.price.toFixed(2);
+
+}
+
+
+
+const p2 = new Product("Monitor", 800.0, 10)
+console.log(p1)
+console.log(p2)
+const p3 = new Product("Mouse", 50.0, 4)
+
