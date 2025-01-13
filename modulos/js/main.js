ffbcac8d0fb05557b6c27ebb85c9c01bb6a1bdd4
Gustavo Ronan <gustavoronn@gmail.com>
2025-01-13 19:25:33 -0300

    finalizando area de modulos

--- modulos/js/main.js
+++ modulos/js/main.js
@@ -1,5 +1,12 @@
-import * as nb from './number.js'
+import { sum, exp, round } from "./number.js"
+import Product from "./product.js"
 
-const x = nb.sum(3,4)
+const x = sum(3,4)
 
-console.log(x)
+console.log(x) //forma 1
+console.log(exp(3,4)) //forma 2, funcao 2
+console.log(round(3.44332, 2)) //forma 2, funcao 3
+
+const obj = new Product("Mousepad", 20.00, 14)
+
+console.log(obj)
--- modulos/js/number.js
+++ modulos/js/number.js
@@ -1,12 +1,14 @@
-export function sum(num1, num2){
+ function sum(num1, num2){
     return num1 + num2
 }
 
-export function exp(base, expoente){
+ function exp(base, expoente){
     return base ** expoente
 }
 
-export function round(value, precision){
+ function round(value, precision){
     var multiplier = exp(10, precision || 0)
     return Math.round(value * multiplier) / multiplier
 }
+
+export{sum, round, exp}
